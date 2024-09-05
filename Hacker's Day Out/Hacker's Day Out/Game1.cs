0704e13e6a845ee1514985407ab2c77e0199e82d
Darius Tyson <dariust@ksu.edu>
2024-09-05 01:51:47 -0500

    Hello world

--- Hacker's Day Out/Hacker's Day Out/Game1.cs
+++ Hacker's Day Out/Hacker's Day Out/Game1.cs
@@ -9,6 +9,8 @@ namespace HackersDayOut
         private GraphicsDeviceManager _graphics;
         private SpriteBatch _spriteBatch;
 
+        private SpriteFont _title;
+
         public Game1()
         {
             _graphics = new GraphicsDeviceManager(this);
@@ -28,6 +30,7 @@ namespace HackersDayOut
             _spriteBatch = new SpriteBatch(GraphicsDevice);
 
             // TODO: use this.Content to load your game content here
+            _title = Content.Load<SpriteFont>("Title");
         }
 
         protected override void Update(GameTime gameTime)
@@ -45,7 +48,9 @@ namespace HackersDayOut
             GraphicsDevice.Clear(Color.CornflowerBlue);
 
             // TODO: Add your drawing code here
-
+            _spriteBatch.Begin();
+            _spriteBatch.DrawString(_title, "Hello World", new Vector2(270, 70), Color.Black);
+            _spriteBatch.End();
             base.Draw(gameTime);
         }
     }
